1c656825dae836340c4af3d27b709c156b8d04f2
v.melekhin <v.melekhin@timeweb.ru>
2026-04-27 16:22:59 +0300

    add post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,18 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
   getRequestBody(@Body() body: unknown): { ok: true } {
-    console.log(body);
+    console.log('getRequestBody', body);
+
+    return { ok: true };
+  }
+
+
+  @Post()
+  postRequestBody(@Body() body: unknown): { ok: true } {
+    console.log('postRequestBody', body)
 
     return { ok: true };
   }
