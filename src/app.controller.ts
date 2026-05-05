ca46d49b1365c03b15aa76a36176a4fbdf7cd323
v.melekhin <v.melekhin@timeweb.ru>
2026-05-05 13:31:11 +0300

    redirect git ref

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,31 +2,32 @@ import {Body, Controller, Get, Post, Redirect} from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get()
-  getRequestBody(@Body() body: unknown): { ok: true } {
-    console.log('getRequestBody', body);
+    @Get()
+    getRequestBody(@Body() body: unknown): { ok: true } {
+        console.log('getRequestBody', body);
 
-    return { ok: true };
-  }
+        return {ok: true};
+    }
 
-  @Get('redirect')
-  @Redirect('https://imposter18-request-checker-5ea4.twc1.net', 302)
-  redirect(): void {
-    console.log('redirect called');
+    @Get('redirect')
+    @Redirect('https://imposter18-request-checker-5ea4.twc1.net', 302)
+    redirect(): void {
+        console.log('redirect called');
 
-  }
+    }
 
-  @Get('redirect.git')
-  @Redirect('https://imposter18-request-checker-5ea4.twc1.net', 302)
-  redirectGit(): void {
-    console.log('redirect called git');
 
-  }
+    @Get('redirect.git/info/refs')
+    @Redirect('https://imposter18-request-checker-5ea4.twc1.net', 302)
+    redirectGitRefs(): void {
+        console.log('redirect called git refs');
 
-  @Post()
-  postRequestBody(@Body() body: unknown): { ok: true } {
-    console.log('postRequestBody', body)
+    }
 
-    return { ok: true };
-  }
+    @Post()
+    postRequestBody(@Body() body: unknown): { ok: true } {
+        console.log('postRequestBody', body)
+
+        return {ok: true};
+    }
 }
